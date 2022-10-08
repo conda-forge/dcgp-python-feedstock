6835caa77ae0c050debbed7800b82c1567ddb6f3
Dario Izzo <dario.izzo@gmail.com>
2022-10-08 12:32:52 +0200

    update pybind incusion

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,21 +11,6 @@ if [[ "$target_platform" == linux-64 ]]; then
     LDFLAGS="-lrt ${LDFLAGS}"
 fi
 
-# Install a Pybind11 version compatible with pagmo and audi
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
-mkdir build
-cd build
-pwd
-cmake \
-    -DPYBIND11_TEST=NO \
-    -DCMAKE_INSTALL_PREFIX=$DCGPY_BUILD_DIR \
-    -DCMAKE_PREFIX_PATH=$DCGPY_BUILD_DIR \
-    ..
-make install
-cd ../..
-
 # Install the dcgp headers first.
 cmake \
     -DBoost_NO_BOOST_CMAKE=ON \
@@ -51,8 +36,7 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DDCGP_BUILD_DCGP=no \
-    -DDCGP_BUILD_DCGPY=yes \
-    -Dpybind11_DIR=$DCGPY_BUILD_DIR/share/cmake/pybind11/ \
+    -DDCGP_BUILD_DCGPY=yes
     ..
 
 make -j${CPU_COUNT} VERBOSE=1
